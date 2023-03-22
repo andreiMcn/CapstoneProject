de49ab9c92f8a759d889a4240e7345f0c8cdcec4
andreiMcn <128433983+andreiMcn@users.noreply.github.com>
2023-03-22 16:16:54 +0200

    Delete Dialogs directory

--- Dialogs/add-new-training-dialog/add-new-training-dialog.enum.ts
+++
@@ -1,6 +0,0 @@
-export enum AddTrainingControlsEnum {
-  NAME = 'name',
-  DESCRIPTION = 'description',
-  START_DATE  = 'start_date',
-  END_DATE = 'end_date',
-}
--- Dialogs/edit-training-dialog/edit-training-dialog.enum.ts
+++
@@ -1,8 +0,0 @@
-export enum EditTrainingControlsEnum {
-  ID = 'id',
-  NAME = 'name',
-  DESCRIPTION = 'description',
-  START_DATE  = 'start_date',
-  END_DATE = 'end_date',
-  ADDED_BY = 'added_by'
-}
